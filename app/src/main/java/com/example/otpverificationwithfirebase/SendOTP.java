579121568e4d23169df6afd618ce697985d2cc3b
SefisoMonama <Sfisomonama998@gmail.com>
2023-09-18 17:22:40 +0200

    get otp backend code

--- app/src/main/java/com/example/otpverificationwithfirebase/SendOTP.java
+++ app/src/main/java/com/example/otpverificationwithfirebase/SendOTP.java
@@ -2,8 +2,10 @@ package com.example.otpverificationwithfirebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.otpverificationwithfirebase.databinding.ActivitySendOtpBinding;
 
@@ -23,6 +25,18 @@ public class SendOTP extends AppCompatActivity {
 
     private void setupUi() {
 
+        //onclick listener on get otp button
+        binding.getOTPButton.setOnClickListener(view -> {
+
+            if (binding.inputMobileEditText.getText().toString().trim().isEmpty()){
+                Toast.makeText(SendOTP.this, "Enter Mobile Number", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            Intent intent = new Intent(getApplicationContext(), VerifyOTPActivity.class);
+            intent.putExtra("mobile", binding.inputMobileEditText.getText().toString());
+            startActivity(intent);
+
+        });
 
     }
 
--- app/src/main/java/com/example/otpverificationwithfirebase/VerifyOTPActivity.java
+++ app/src/main/java/com/example/otpverificationwithfirebase/VerifyOTPActivity.java
@@ -3,7 +3,10 @@ package com.example.otpverificationwithfirebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.otpverificationwithfirebase.databinding.ActivityVerifyOtpactivityBinding;
 
@@ -22,5 +25,102 @@ public class VerifyOTPActivity extends AppCompatActivity {
 
     private void setupUI(){
 
+        binding.moblieNumberTextView.setText(String.format(
+                "+27-%s",getIntent().getStringExtra("mobile")
+        ));
+       setupOTPInputs();
+    }
+
+    private void setupOTPInputs(){
+        binding.inputCode1EditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (!charSequence.toString().trim().isEmpty()){
+                    binding.inputCode2EditText.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        binding.inputCode2EditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (!charSequence.toString().trim().isEmpty()){
+                    binding.inputCode3EditText.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        binding.inputCode3EditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (!charSequence.toString().trim().isEmpty()){
+                    binding.inputCode4EditText.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        binding.inputCode4EditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (!charSequence.toString().trim().isEmpty()){
+                    binding.inputCode5EditText.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        binding.inputCode5EditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (!charSequence.toString().trim().isEmpty()){
+                    binding.inputCode6EditText.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 }
